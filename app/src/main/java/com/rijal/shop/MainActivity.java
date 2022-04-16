0cf58a87074f47ae171577b168622f41a84c8515
Rizal Abdul Basith <rizalgu4rd14n@gmail.com>
2022-04-16 14:07:06 +0700

    Initial commit

---
+++ app/src/main/java/com/rijal/shop/MainActivity.java
@@ -0,0 +1,20 @@
+package com.rijal.shop;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button btnComputer, btnPhone, btnHomeServices;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        btnComputer = (Button) findViewById(R.id.btnComputer);
+        btnPhone = (Button) findViewById(R.id.btnPhone);
+        btnHomeServices = (Button) findViewById(R.id.btnHomeServices);
+
+    }
+}
